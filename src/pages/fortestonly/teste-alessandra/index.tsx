f7ed37f30ffec197d53733a45383e81096d26939
gabrielvsouza97 <gabrievasconcelos@gmail.com>
2022-10-26 20:20:10 -0300

    fix: corrigindo importação de temas

--- src/pages/fortestonly/teste-alessandra/index.tsx
+++ src/pages/fortestonly/teste-alessandra/index.tsx
@@ -1,10 +1,8 @@
-import ParticleText from "components/particles/particle-text"
-import {THEME} from "../../../constants"
-export default function TesteAlessandra () {
-    return (
-        <>
-
-      <ParticleText tagType="h1" colorText={THEME.light.colors.primary2} fontWeight="700" fontSize="64">
+import ParticleText from "components/particles/particle-text";
+export default function TesteAlessandra() {
+  return (
+    <>
+      <ParticleText tagType="h1" fontWeight="700" fontSize="64">
         Página Alessandra
       </ParticleText>
         </>
