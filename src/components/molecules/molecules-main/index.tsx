f52abadf8a847af82a16de4c651b6edf4ebe3a4c
Bruno Vieira <brunosvo@yahoo.com.br>
2022-10-26 20:47:02 -0300

    feat: ajustando o tamanho e posicao da main

---
+++ src/components/molecules/molecules-main/index.tsx
@@ -0,0 +1,7 @@
+import * as Styled from './molecules-main-style';
+
+export default function (props: any){
+  return(
+    <Styled.StyledMain>{props.children}</Styled.StyledMain>
+  );
+}
--- src/pages/fortestonly/teste-bruno/index.tsx
+++ src/pages/fortestonly/teste-bruno/index.tsx
@@ -4,20 +4,19 @@ import ParticleImg from "components/particles/particle-img"
 import ParticleText from "components/particles/particle-text"
 import {THEME} from "../../../constants"
 import ParticleDiv from "components/particles/particle-div"
+import MoleculesMain from "components/molecules/molecules-main"
+
 export default function TesteBruno () {
     return (
         <>
         <ParticleDiv>
-        <ParticleImg imgLink={logo} width="200px"></ParticleImg>
-
-      </ParticleDiv>
-
-
-      <div></div>
-
-<ParticleText tagType="h1" colorText={THEME.light.colors.primary2} fontWeight="700" fontSize="64">
-  Página Bruno
-</ParticleText>
+          <ParticleImg imgLink={logo} width="200px"></ParticleImg>
+        </ParticleDiv>
+        <MoleculesMain>
+          <ParticleText tagType="h1" colorText={THEME.light.colors.primary2} fontWeight="700" fontSize="64">
+          Página Bruno
+          </ParticleText>
+        </MoleculesMain>
         </>
     )
 }
