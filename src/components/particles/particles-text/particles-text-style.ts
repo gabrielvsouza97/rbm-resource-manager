aaa09e96a04fd318448013b0d07ad413ab96a21a
gabrielvsouza97 <gabrievasconcelos@gmail.com>
2022-10-26 00:57:43 -0300

    feat: terminando a estilização e props do componen

--- src/components/particles/particles-text/particles-text-style.ts
+++ src/components/particles/particles-text/particles-text-style.ts
@@ -1,5 +1,17 @@
 import styled from "styled-components";
 
-export const SpanInit = styled.span<{ colorText: string }>`
-  color: ${({ colorText }) => colorText};
+type StylesTextComponent = {
+  colorText?: string;
+  fontWeight?: string;
+  fontSize?: string;
+  lineHeight?: string;
+  textAlign?: string;
+};
+
+export const SpanInit = styled.span<StylesTextComponent>`
+  color: ${(props) => (props.colorText ? props.colorText : "#262424")};
+  font-weight: ${(props) => (props.fontWeight ? props.fontWeight : 400)};
+  font-size: ${(props) => (props.fontSize ? props.fontSize : 14)}px;
+  line-height: ${(props) => (props.lineHeight ? props.lineHeight : 21)}px;
+  text-align: ${(props) => (props.textAlign ? props.textAlign : "left")};
 `;
