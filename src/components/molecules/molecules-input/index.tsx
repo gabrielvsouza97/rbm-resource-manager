f039e00e3030fcf1a37698900abff3e7f399c128
a.srodrigues <alessandra.rodrigues@rbmweb.com.br>
2022-10-25 23:50:33 -0300

    feat/ajustes nos Theme e exclusão do index desnecessário

---
+++ src/components/molecules/molecules-input/index.tsx
@@ -0,0 +1,7 @@
+import * as Styled from './molecules-input-style';
+
+export default function Input(){
+  return(
+    <Styled.StyledInput></Styled.StyledInput>
+  );
+}
---
+++ src/components/molecules/molecules-input/molecules-input-style.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const StyledInput = styled.input`
+  background-color: blue;
+  color: #FFFFFF;
+  border: 1px solid red;
+  height: 54px;
+  width: 410px;
+  border-radius: 6px;
+  border-color: #CED4DA;
+`;
