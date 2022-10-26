0d310c322e6d77f4afb2345afd3bb00cd7c4dc6a
gabrielvsouza97 <gabrievasconcelos@gmail.com>
2022-10-26 20:20:10 -0300

    fix: corrigindo importação de temas

--- src/pages/fortestonly/teste-gabriel/index.tsx
+++ src/pages/fortestonly/teste-gabriel/index.tsx
@@ -1,45 +1,40 @@
-import ParticleText from "components/particles/particle-text"
-import { ThemeProps, ThemeProvider } from "styled-components"
-import {THEME} from "../../../constants"
+import ParticleText from "components/particles/particle-text";
 
-type ThemeContextProps = {
-  colors: ColorsTheme
-}
-
-type ColorsTheme = {
-  primary1: string;
-  primary2:string;
-  primary3:string;
-  secondary1: string;
-  secondary2:string;
-  secondary3:string;
-}
-export default function TesteGabriel (props:{theme: ThemeContextProps}) {
-  const Pessoas = ["Alessandra", "Pedro", "Davi", "Rhuan", "Gabriel"]
-    return (
-        <>
-
-      <ParticleText tagType="h1" colorText={props.theme.colors.primary2} fontWeight="700" fontSize="64">
+const Pessoas = ["Alessandra", "Pedro", "Davi", "Rhuan", "Gabriel"];
+export default function TesteGabriel() {
+  return (
+    <>
+      <ParticleText
+        tagType="h1"
+        colorText={"primary2"}
+        fontWeight="700"
+        fontSize="64"
+        for="email"
+      >
         Loren Ipsun
       </ParticleText>
-      <ParticleText tagType="h2" colorText={props.theme.colors.secondary2} fontSize="36">
+      <ParticleText tagType="h2" colorText={"secondary2"} fontSize="36">
         Feito com 💙 e ☕
       </ParticleText>
-      <ParticleText tagType="span" colorText={props.theme.colors.primary1} >
+      <ParticleText tagType="span" colorText={"primary1"}>
         Alessandra ● Bruno ● Davi ● Eduardo ● Gabriel ● Victor
       </ParticleText>
-      <ParticleText tagType="p" colorText={props.theme.colors.secondary2} >
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos doloribus, neque dolorem libero corporis doloremque totam cumque eveniet repellendus cupiditate! Itaque blanditiis explicabo sequi eum fugiat ipsa quasi eligendi. Ut.
+      <ParticleText tagType="p" colorText={"secondary2"}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+        doloribus, neque dolorem libero corporis doloremque totam cumque eveniet
+        repellendus cupiditate! Itaque blanditiis explicabo sequi eum fugiat
+        ipsa quasi eligendi. Ut.
       </ParticleText>
 
-      <ParticleText tagType="p">
-        Grupo de estudos
-      </ParticleText>
+      <ParticleText tagType="p">Grupo de estudos</ParticleText>
       <ul>
-        {Pessoas.map((item, index) => 
-          <ParticleText tagType="li" key={index} colorText={props.theme.colors.primary1}> {item}</ParticleText>
-        )}
+        {Pessoas.map((item, index) => (
+          <ParticleText tagType="li" key={index} colorText={"secondary2"}>
+            {" "}
+            {item}
+          </ParticleText>
+        ))}
       </ul>
-        </>
-    )
+    </>
+  );
 }
