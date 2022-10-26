8be093c81950099094d149accff6a3c259a9bfd6
a.srodrigues <alessandra.rodrigues@rbmweb.com.br>
2022-10-26 19:06:49 -0300

    feat/alteracoesDaDev

--- src/components/molecules/molecules-input/molecules-input-style.ts
+++ src/components/molecules/molecules-input/molecules-input-style.ts
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 export const StyledInput = styled.input`
   color: #ced4da;
-  border: 1px solid red;
   height: 54px;
   width: 410px;
   background: #ffffff;
   border: 1px solid #ced4da;
   border-radius: 6px;
+  :focus{
+    border-color: #0092D4;
+  }
 `;
+
