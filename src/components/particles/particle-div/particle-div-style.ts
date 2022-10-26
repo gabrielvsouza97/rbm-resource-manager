43d5ae7d2d0837767d3f7e213fa86cfb66241a34
Bruno Vieira <brunosvo@yahoo.com.br>
2022-10-26 20:24:54 -0300

    feat: header e logo

---
+++ src/components/particles/particle-div/particle-div-style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const StyledDiv2 = styled.div `
+    background-color: green;
+    width: 100%;
+    height: 76px;
+
+`;
--- src/pages/fortestonly/teste-bruno/index.tsx
+++ src/pages/fortestonly/teste-bruno/index.tsx
@@ -1,12 +1,23 @@
+import logo from "../../../assets/img/logo.svg"
+
+import ParticleImg from "components/particles/particle-img"
 import ParticleText from "components/particles/particle-text"
 import {THEME} from "../../../constants"
+import ParticleDiv from "components/particles/particle-div"
 export default function TesteBruno () {
     return (
         <>
+        <ParticleDiv>
+        <ParticleImg imgLink={logo} width="200px"></ParticleImg>
 
-      <ParticleText tagType="h1" colorText={THEME.light.colors.primary2} fontWeight="700" fontSize="64">
-        Página Bruno
-      </ParticleText>
+      </ParticleDiv>
+
+
+      <div></div>
+
+<ParticleText tagType="h1" colorText={THEME.light.colors.primary2} fontWeight="700" fontSize="64">
+  Página Bruno
+</ParticleText>
         </>
     )
 }
