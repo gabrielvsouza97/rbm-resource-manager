86cfb7c63df52f3766f4be33da1c0996bb3faa91
a.srodrigues <alessandra.rodrigues@rbmweb.com.br>
2022-10-26 22:19:08 -0300

    feat/pull da dev

--- src/pages/fortestonly/teste-alessandra/index.tsx
+++ src/pages/fortestonly/teste-alessandra/index.tsx
@@ -1,12 +1,12 @@
-<<<<<<< HEAD
+
 import InputText from "components/molecules/molecules-input"
 import ParticleText from "components/particles/particle-text"
-import {THEME} from "../../../constants"
+
 export default function TesteAlessandra () {
     return (
         <>
 
-      <ParticleText tagType="h1" colorText={THEME.light.colors.primary2} fontWeight="700" fontSize="64">
+<ParticleText tagType="h1" fontWeight="700" fontSize="64">
         Página Alessandra
       </ParticleText>
       <InputText placeholder="Digite seu email"></InputText>
@@ -14,15 +14,3 @@ export default function TesteAlessandra () {
         </>
     )
 }
-=======
-import ParticleText from "components/particles/particle-text";
-export default function TesteAlessandra() {
-  return (
-    <>
-      <ParticleText tagType="h1" fontWeight="700" fontSize="64">
-        Página Alessandra
-      </ParticleText>
-    </>
-  );
-}
->>>>>>> 8452184e650fb2e672f6fd894b4293b77a24d292
