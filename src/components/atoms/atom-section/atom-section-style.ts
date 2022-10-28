4b0ed90b69026c308b52b521c889b82541e0e569
a.srodrigues <alessandra.rodrigues@rbmweb.com.br>
2022-10-27 23:01:56 -0300

    feat/pull da dev

---
+++ src/components/atoms/atom-section/atom-section-style.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledSection = styled.section`
+  height: calc(100vh - 30px);
+  width: 45vw;
+  margin: 30px;
+`;
+
+export const StyledDiv = styled.div`
+  background-color: aliceblue;
+  height:90%;
+  width: 100%;
+  padding: 15px;
+  text-align: start;
+  display: grid;
+  align-items: flex-start;
+`;
+
--- src/components/molecules/molecules-main/index.tsx
+++ src/components/molecules/molecules-main/index.tsx
@@ -1,6 +1,6 @@
 import * as Styled from './molecules-main-style';
 
-export default function (props: any){
+export default function Main(props: any){
   return(
     <Styled.StyledMain>{props.children}</Styled.StyledMain>
   );
