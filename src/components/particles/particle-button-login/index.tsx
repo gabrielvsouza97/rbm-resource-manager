cb90a4fa34f2d42429602a438bfaf650a59dcb34
DaviMontan <davimontan77@gmail.com>
2022-11-01 18:07:42 -0300

    fix: iniciando estrutura da função onclick

--- src/components/particles/particle-button-login/index.tsx
+++ src/components/particles/particle-button-login/index.tsx
@@ -1,15 +1,53 @@
+import axios from "axios";
 import { ReactElement, ReactNode, useState } from "react";
+import { useNavigate } from "react-router";
 import * as Styled from "./particle-button-style";
 
 type ItemsButtonProps = {
-    children : ReactNode;
+    children: ReactNode;
     light?: boolean;
     dark?: boolean;
-}
+};
 
+export default function ButtonLogin(props: ItemsButtonProps) {
 
+    const navigate = useNavigate();
 
-export default function ButtonLogin(props : ItemsButtonProps){
+    async function Logando(
+        loginInput: string | undefined,
+        senhaInput: string | undefined
+    ) {
 
-    return <Styled.ButtonStyled light={props.light} dark={props.dark}>{props.children}</Styled.ButtonStyled>;
+        if (loginInput && senhaInput) {
+            if (loginInput.length > 0 && senhaInput.length > 0) {
+                //login = "admin1"
+                //senha = "R@12b23m34!"
+                try {
+                    let requisicao = await axios.post(
+                        "https://agendamento.carloschoma.com.br/login",
+                        {
+                            login: loginInput,
+                            senha: senhaInput,
+                        }
+                    );
+                    console.log(requisicao.data); 
+                    navigate("/gabriel");
+                    // login(loginInput, senhaInput)
+                } catch {
+                    console.log("login/senha incorreta");
+                }
+            }
+        } else {
+        }
+    }
+
+    return (
+        <Styled.ButtonStyled
+            onClick={() => Logando('admin1','R@12b23m34!')}
+            light={props.light}
+            dark={props.dark}
+        >
+            {props.children}
+        </Styled.ButtonStyled>
+    );
 }
