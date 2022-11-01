e033744be2c4d553292cfa040f05ed55ce1ec2cc
DaviMontan <davimontan77@gmail.com>
2022-11-01 00:13:29 -0300

    fix: subindo modificações nos componentes do botão

--- src/components/particles/particle-button-login/particle-button-style.ts
+++ src/components/particles/particle-button-login/particle-button-style.ts
@@ -1,14 +1,29 @@
-import styled from "styled-components";
+import { THEME } from "../../../constants";
+import { type } from "os";
+import styled, { css } from "styled-components";
+type LoginButton = {
+    light ?: boolean;
+    dark ?: boolean; 
+}
+const StyleLight = css`
+    background-color: ${THEME.light.colors.primary2};
+`
+const StyleDark= css`
+    background-color: ${THEME.dark.colors.primary2};
+`
 
-export const ButtonStyled = styled.button`
-    width: 410px;
-    height: 58px;
+export const ButtonStyled = styled.button<LoginButton>`
+    min-width: 410px;
+    min-height: 58px;
 
-    background: #0092D4;
+    ${({light})=> StyleLight};
+    ${({dark})=> StyleDark};
     border-radius: 6px;
     border: none;
 
     color: white;
     font-weight: 600;
     font-size: 16px;
+    cursor: pointer;
+    text-transform: uppercase;
 `
